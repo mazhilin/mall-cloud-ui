54273febc4b956279d9dede8a185be7fb8b3b83e
marklin <marklin1992@outlook.com>
2020-11-11 09:28:22 +0800

    main[2020-11-11 星期三 09:26]:商城云UI平台-管理后台提交骨架

---
+++ mall-console-ui/component/hui-admin-2.5/js/page.js
@@ -0,0 +1,127 @@
+(function($){
+    var ms = {
+        init:function(obj,args){
+            return (function(){
+                ms.fillHtml(obj,args);
+                ms.bindEvent(obj,args);
+            })();
+        },
+        //填充html
+        fillHtml:function(obj,args){
+            return (function(){
+                obj.empty();
+                //上一页
+                if(args.current > 1){
+                    obj.append('<a href="javascript:;" class="prevPage">上一页</a>');
+                }else{
+                    obj.remove('.prevPage');
+                    //obj.append('<span class="disabled"></span>');
+                }
+                //中间页码
+                if(args.current != 1 && args.current >= 4 && args.pageCount != 4){
+                    obj.append('<a href="javascript:;" class="tcdNumber">'+1+'</a>');
+                }
+                if(args.current-2 > 2 && args.current <= args.pageCount && args.pageCount > 5){
+                    obj.append('<span style="margin:0 5px;">...</span>');
+                }
+                var start = args.current -2,end = args.current+2;
+                if((start > 1 && args.current < 4)||args.current == 1){
+                    end++;
+                }
+                if(args.current > args.pageCount-4 && args.current >= args.pageCount){
+                    start--;
+                }
+                for (;start <= end; start++) {
+                    if(start <= args.pageCount && start >= 1){
+                        if(start != args.current){
+                            obj.append('<a href="javascript:;" class="tcdNumber">'+ start +'</a>');
+                        }else{
+                            obj.append('<span class="current">'+ start +'</span>');
+                        }
+                    }
+                }
+                if(args.current + 2 < args.pageCount - 1 && args.current >= 1 && args.pageCount > 5){
+                    obj.append('<span style="margin:0 5px;">...</span>');
+                }
+                if(args.current != 1 && args.current != args.pageCount && args.current < args.pageCount -2  && args.pageCount != 4){
+                    obj.append('<a href="javascript:;" class="tcdNumber">'+args.pageCount+'</a>');
+                }
+                //下一页
+                if(args.current < args.pageCount){
+                    obj.append('<a href="javascript:;" class="nextPage">下一页</a>');
+                }else{
+                    obj.remove('.nextPage');
+                    obj.append('<span class="disabled"></span>');
+                }
+            })();
+        },
+        //绑定事件
+        bindEvent:function(obj,args){
+            return (function(){
+                obj.on("click","a.tcdNumber",function(){
+                    var current = parseInt($(this).text());
+                    ms.fillHtml(obj,{"current":current,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current);
+                    }
+                });
+                //上一页
+                obj.on("click","a.prevPage",function(){
+                    var current = parseInt(obj.children("span.current").text());
+                    ms.fillHtml(obj,{"current":current-1,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current-1);
+                    }
+                });
+                //下一页
+                obj.on("click","a.nextPage",function(){
+                    var current = parseInt(obj.children("span.current").text());
+                    ms.fillHtml(obj,{"current":current+1,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current+1);
+                    }
+                });
+            })();
+        }
+    }
+    $.fn.createPage = function(options){
+        var args = $.extend({
+            pageCount : 15,
+            current : 1,
+            backFn : function(){}
+        },options);
+        ms.init(this,args);
+    }
+})(jQuery);
+function dateFtt(fmt,date)  { 
+  var o = {   
+    "M+" : date.getMonth()+1,                 //月份   
+    "d+" : date.getDate(),                    //日   
+    "h+" : date.getHours(),                   //小时   
+    "m+" : date.getMinutes(),                 //分   
+    "s+" : date.getSeconds(),                 //秒   
+    "q+" : Math.floor((date.getMonth()+3)/3), //季度   
+    "S"  : date.getMilliseconds()             //毫秒   
+  };   
+  if(/(y+)/.test(fmt))   
+    fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
+  for(var k in o)   
+    if(new RegExp("("+ k +")").test(fmt))   
+  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+  return fmt;   
+} 
+//创建时间格式化显示
+function crtTimeFtt(value){
+	if(value == "" || value == null){
+		return "";
+	}
+    var crtTime = new Date(value);
+    return dateFtt("yyyy-MM-dd hh:mm:ss",crtTime);//直接调用公共JS里面的时间类处理的办法     
+}
+//回车事件绑定
+    $('#searchKey').bind('keyup', function(event) {
+        if (event.keyCode == "13") {
+            //回车执行查询
+            $('#searchKeySubmit').click();
+        }
+    });
---
+++ mall-console-ui/public/js/utils.js
@@ -0,0 +1,242 @@
+var utils = {
+    //baseUrl: 'http://47.104.133.96/shaker_test',
+    passportApiUrl: 'http://127.0.0.1:9001/passport',
+    consoleApiUrl: 'http://127.0.0.1:9001/console',
+    //baseUrl: 'http://127.0.0.1:8008/shaker',
+    updateBy: function() {
+        return window.localStorage.getItem('updateBy');
+    },
+    isChecked: function() {
+        var token = window.localStorage.getItem('token');
+        if(token){
+            return token;
+        }else{
+            return "";
+        }
+    },
+    //baseUrl: '',
+    //获取url链接参数
+    getQueryString: function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if(r != null) return r[2];
+        return '';
+    },
+    // 转换成2018年9月27号 11:00:00
+    formatTime: function (t) {
+        var date = new Date(t);
+        var y = date.getFullYear(),
+            m = date.getMonth() + 1,
+            d = date.getDate(),
+            h = ('0' + date.getHours()).slice(-2),
+            mi = ('0' + date.getMinutes()).slice(-2),
+            s = ('0' + date.getSeconds()).slice(-2);
+        return (y + '年' + m + '月'+ d + '日' + h + ':' + mi + ':' + s)
+    },
+    formatTime2: function (t) {
+        var date = new Date(t);
+        var y = date.getFullYear(),
+            m = date.getMonth() + 1,
+            d = date.getDate(),
+            h = ('0' + date.getHours()).slice(-2),
+            mi = ('0' + date.getMinutes()).slice(-2),
+            s = ('0' + date.getSeconds()).slice(-2);
+        return (y + '-' + m + '-'+ d + ' ' + h + ':' + mi + ':' + s)
+    },
+    // 转换成2017-09-19
+    format: function(date, format) {
+        var A = Array();
+        var currtime = new Date(date);
+        A.push(currtime.getFullYear());
+        A.push(('0' + (currtime.getMonth() + 1)).slice(-2));
+        A.push(('0' + currtime.getDate()).slice(-2));
+        return A.join(format || '-');
+    },
+    //2019-02-02 10:19:56
+    //参数fmt "yyyy-MM-dd hh:mm:ss" 参数date 时间戳
+    //参数fmt "yyyy-MM-dd EEE hh:mm:ss" 2019-02-19 星期二 14:04:30
+    dateFtt: function (fmt, date) {
+        if(date==undefined){
+            return '';
+        }else{
+            var date = new Date(date);
+        }
+        var o = {
+            "M+": date.getMonth() + 1,                 //月份
+            "d+": date.getDate(),                    //日
+            "h+": date.getHours(),                   //24小时
+            "H+": date.getHours() % 12 == 0 ? 12 : date.getHours() % 12, //12小时
+            "m+": date.getMinutes(),                 //分
+            "s+": date.getSeconds(),                 //秒
+            "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+            "S": date.getMilliseconds()             //毫秒
+        };
+        var week = {
+            "0": "日",
+            "1": "一",
+            "2": "二",
+            "3": "三",
+            "4": "四",
+            "5": "五",
+            "6": "六"
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        if (/(E+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "星期" : "周") : "") + week[date.getDay() + ""]);
+        }
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    },
+    //添加Cookie
+    addCookie: function (name, value, expireHours) {
+        var cookieString = name + "=" + escape(value) + "; path=/";
+        //判断是否设置过期时间
+        if(expireHours > 0) {
+            var date = new Date();
+            date.setTime(date.getTime() + expireHours * 3600 * 1000);
+            cookieString = cookieString + ";expires=" + date.toGMTString();
+        }
+        document.cookie = cookieString;
+    },
+    //获取Cookie
+    getCookie:function(name){
+        var strcookie = document.cookie;
+        var arrcookie = strcookie.split("; ");
+        for(var i = 0; i < arrcookie.length; i++) {
+            var arr = arrcookie[i].split("=");
+            if(arr[0] == name) return unescape(arr[1]);
+        }
+        return null;
+    },
+    //删除Cookie
+    delCookie:function(name){
+        this.addCookie(name,1,-1)
+    },
+    //判断对象是否为空
+    isEmpty:function(obj) {
+        for(var name in obj) {
+            return false;
+        }
+        return true;
+    },
+    ajax: function (url, data, successCb, errorCb) {
+        layui.use('jquery', function() {
+            var $ = layui.$;
+            $.getJSON(url, data || {}, successCb);
+        })
+
+    },
+    // 删除对象中的某些属性
+    delKeys: function(obj, keys) {
+        keys.map(function(key) {
+            delete obj[key];
+        })
+        return obj;
+    },
+    optionNotEmpty: function(obj) {
+        var flag = true;
+        for(var key in obj) {
+            if(obj[key]){
+                flag = false;
+                return flag;
+            }
+        }
+    },
+    closeCurropen: function() {
+        // 在当前iframe => 关闭当前iframe
+        var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+        parent.layer.close(index); //再执行关闭
+    }
+}
+// 关闭当前打开iframe
+$('body').on('click', '#closeOpen', function() {
+    utils.closeCurropen();
+})
+$.ajaxSettings.beforeSend = function(request, xhr) {
+    if(xhr.url.indexOf('login') <= -1){
+        var token = utils.isChecked();
+        request.setRequestHeader('token', token)
+    }else{
+        console.log(request);
+    }
+}
+function ListPage(options) {
+    this.page = options.page || 10;
+    this.rows = options.rows || 1;
+    this.listSize = options.listSize || 20;
+    this.url = options.url;
+    this.listName = options.listName || 'dataList';
+    this.vm = options.vm;
+    this.callback = options.callback || '';
+    this.ajax();
+}
+ListPage.prototype = {
+    ajax: function () {
+        var _this = this;
+        layui.use(['jquery', 'layer'], function() {
+            var $ = layui.$;
+            $.ajax({
+                url: _this.url,
+                data: {
+                    page: _this.page,
+                    rows: _this.rows
+                },
+                async: false
+            }).done(function(res) {
+                if(res.code == 200){
+                    _this.listSize = res.result.listSize;
+                    _this.vm.table = res.result.obj[_this.listName];
+                    if(typeof _this.callback == 'function') _this.callback(_this);
+                    // if(_this.page == 1){
+                    _this.init(_this.vm.table);
+                    // }
+
+                }else if(res.code == '500404'){
+                    top.location.href = utils.baseUrl + '/manager/view/login.html-b'
+                }else{
+                    layer.msg(res.message);
+
+                }
+            }).fail(function(xhr) {
+                console.log(xhr);
+            })
+        })
+    },
+    init: function(datalist) {
+        var _this = this;
+        layui.use('laypage', function() {
+            var laypage = layui.laypage;
+            laypage.render({
+                elem: 'list-page',
+                count: _this.listSize,
+                limit: _this.rows,
+                curr: _this.page,
+                layout: ['count', 'prev', 'page', 'next', 'limit', 'skip'],
+                jump: function (obj, first) {
+                    console.log(obj)
+                    _this.page = obj.curr;
+                    if(!first){
+                        _this.ajax();
+                    }
+                }
+            })
+        })
+    }
+}
+
+if(top.location.href.indexOf('login') <= -1){
+    $.get(utils.consoleApiUrl + "/api/console/home/isLogin", {}, function (response) {
+        if (response.code == 200) {
+            console.log("登陆验证成功")
+        } else if (response.code == '800') {
+            top.location.href = utils.baseUrl + '/manager/view/login.html-b';
+        }
+    })
+}
+
+
+
+
